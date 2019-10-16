fe6db7abdd879a3bbd077899bd5f27c703b17b16
nielinjie <nielinjie@gmail.com>
2019-10-16 10:34:54 +0800

    wrap

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
 export * from './src/dataProvider/DataProviders';
 export * from './src/dataProvider/dataFetchActions';
+import * as w from './src/dataProvider/Wrapper';
+export { w };
--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,16 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./src/dataProvider/DataProviders"));
 __export(require("./src/dataProvider/dataFetchActions"));
+const w = __importStar(require("./src/dataProvider/Wrapper"));
+exports.w = w;
 //# sourceMappingURL=index.js.map
