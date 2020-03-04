8f4eb6086dd9ef15c899fb0781eb7f5f1be09809
nielinjie <nielinjie@gmail.com>
2020-03-04 14:57:17 +0800

    sfp

--- src/dataProvider/Wrapper.ts
+++ src/dataProvider/Wrapper.ts
@@ -1,42 +1,70 @@
-import { DataProvider, chain as c, withStaticData as wsd, withDynamicData as wdd, DataProviderParams, NotCovered, forResourceAndFetchType as raf } from './DataProviders';
-import _ from 'lodash';
+import {
+  DataProvider,
+  chain as c,
+  withStaticData as wsd,
+  withDynamicData as wdd,
+  DataProviderParams,
+  NotCovered,
+  forResourceAndFetchType as raf,
+  DataProviderResult,
+  fake
+} from "./DataProviders";
+import _ from "lodash";
+import { GET_LIST } from "./dataFetchActions";
 
 class DataProviderWrap implements DataProviderWrap {
-    private _dp: DataProvider
-    constructor(dataProvider: DataProvider) {
-        this._dp = dataProvider
+  private _dp: DataProvider;
+  constructor(dataProvider: DataProvider) {
+    this._dp = dataProvider;
+  }
+  value(): DataProvider {
+    return this._dp;
+  }
+  chain(dp: DataProvider | DataProviderWrap): DataProviderWrap {
+    if (dp instanceof DataProviderWrap) {
+      return new DataProviderWrap(c(this._dp, dp.value()));
     }
-    value(): DataProvider {
-        return this._dp
-    }
-    chain(dp: DataProvider | DataProviderWrap): DataProviderWrap {
-        if (dp instanceof DataProviderWrap) {
-            return new DataProviderWrap(c(this._dp, dp.value()))
-        }
-        return new DataProviderWrap(c(this._dp, dp))
-    }
-    forResourceAndFetchType(resource: string | string[] | undefined, type: string | string[] | undefined) {
-        return new DataProviderWrap(raf(resource, type, this._dp))
-    }
+    return new DataProviderWrap(c(this._dp, dp));
+  }
+  forResourceAndFetchType(
+    resource: string | string[] | undefined,
+    type: string | string[] | undefined
+  ) {
+    return new DataProviderWrap(raf(resource, type, this._dp));
+  }
 }
 export const emptyDataProvider: DataProvider = (
-         type: string,
-         resource: string,
-         param: DataProviderParams<unknown>
-       ) => {
-         throw new NotCovered("from emptyDataProvider");
-       };
-
-
+  type: string,
+  resource: string,
+  param: DataProviderParams<unknown>
+) => {
+  throw new NotCovered("from emptyDataProvider");
+};
 
 export function withDynamicData(fun: () => any): DataProviderWrap {
-    return new DataProviderWrap(wdd(fun))
+  return new DataProviderWrap(wdd(fun));
 }
-export function withStaticData(data:any):DataProviderWrap{
-    return new DataProviderWrap(wsd(data))
+export function withStaticData(data: any): DataProviderWrap {
+  return new DataProviderWrap(wsd(data));
 }
-export function w(dataProvider: DataProvider = emptyDataProvider): DataProviderWrap {
-    return new DataProviderWrap(dataProvider)
+export function w(
+  dataProvider: DataProvider = emptyDataProvider
+): DataProviderWrap {
+  return new DataProviderWrap(dataProvider);
 }
 
-
+export function localSFP(raw: DataProvider): DataProvider {
+  return (
+    fetchType: string,
+    resource: string,
+    params: DataProviderParams<any>
+  ): Promise<DataProviderResult<any>> => {
+    if (fetchType !== GET_LIST) {
+      throw new Error("localSFP is only for GET_LIST");
+    }
+    const data = raw(fetchType, resource, params);
+    return data.then(result => {
+      return fake({ [resource]: result.data })(fetchType, resource, params);
+    });
+  };
+}
